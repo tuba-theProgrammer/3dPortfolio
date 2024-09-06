0fd60d6f63a8de3d4ebc4f72f6b31dfadc8f662c
unknown <tubarajput92@gmail.com>
2024-09-07 03:47:26 +0500

    final fixes

--- src/app/_common/_components/Tech/index.tsx
+++ src/app/_common/_components/Tech/index.tsx
@@ -1,9 +1,7 @@
-
 import React from "react";
 import { technologies } from "../../_enums/Data/index";
 import { SectionWrapper } from "../../_enums/hoc";
 import Tilt from "react-parallax-tilt";
-
 
 // Define the type for the technology object
 interface Technology {
@@ -14,16 +12,22 @@ interface Technology {
 const Tech: React.FC = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology: Technology, index) => (
-                <Tilt tiltMaxAngleX={45} tiltMaxAngleY={45} perspective={1000} scale={1}>
-            <div className="w-28 h-28 bg-tertiary rounded-[25px]">
-              <img
-                alt={technology.name}
-                src={technology.icon}
-                           />
-            </div>
-          </Tilt>
-
+      {technologies.map((technology: Technology) => (
+        <Tilt
+          key={technology.name}
+          tiltMaxAngleX={45}
+          tiltMaxAngleY={45}
+          perspective={1000}
+          scale={1}
+        >
+          <div className="w-28 h-28 bg-tertiary rounded-[25px] relative">
+            <img 
+              alt={technology.name}
+              src={technology.icon}
+          
+            />
+          </div>
+        </Tilt>
       ))}
     </div>
   );
