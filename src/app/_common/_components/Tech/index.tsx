928c4621a22e3941151b246f06cffff2200139b1
unknown <tubarajput92@gmail.com>
2024-09-07 03:33:47 +0500

    final fixes

--- src/app/_common/_components/Tech/index.tsx
+++ src/app/_common/_components/Tech/index.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import React from "react";
 import { technologies } from "../../_enums/Data/index";
 import { SectionWrapper } from "../../_enums/hoc";
 import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
-import { fadeIn} from "../../_enums/utils/motion";
+import { fadeIn } from "../../_enums/utils/motion";
 
 // Define the type for the technology object
 interface Technology {
@@ -14,18 +15,18 @@ interface Technology {
 const Tech: React.FC = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology: Technology,index) => (
-          <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-          <Tilt
-            tiltMaxAngleX={45}
-            tiltMaxAngleY={45}
-            perspective={1000}
-            scale={1}
-          >
-        <div className="w-28 h-28 bg-tertiary rounded-[25px]" key={technology.name}>
-          <img alt="tech" src={technology.icon} />
-        </div>
-        </Tilt>
+      {technologies.map((technology: Technology, index) => (
+        <motion.div key={technology.name} variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+          <Tilt tiltMaxAngleX={45} tiltMaxAngleY={45} perspective={1000} scale={1}>
+            <div className="w-28 h-28 bg-tertiary rounded-[25px]">
+              <Image
+                alt={technology.name}
+                src={technology.icon}
+                layout="fill" // Ensures the image fills the parent container
+                objectFit="contain" // Adjusts how the image fits within its container
+              />
+            </div>
+          </Tilt>
         </motion.div>
       ))}
     </div>
